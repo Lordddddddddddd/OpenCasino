1c7495afb2967ecc7dea4b964780c817ec755674
Lordddddddddddd <nikicn701@gmail.com>
2025-04-11 15:33:37 +0300

    Update and rename lua.lua to 228

--- lua.lua
+++
@@ -1,7 +0,0 @@
-local internet = require("internet")
-local file = io.open("/home/image.png", "wb")
-for chunk in internet.request("https://github.com/Lordddddddddddd/OpenCasino/blob/main/image.png") do
-    file:write(chunk)
-end
-file:close()
-print("Изображение успешно загружено!")
