414dc01a5ac9fc0a0f303e1b5fcd2ef34b922d7f
Lordddddddddddd <nikicn701@gmail.com>
2025-04-11 14:52:08 +0300

    Update casino.lua

--- casino.lua
+++ casino.lua
@@ -1,10 +1,17 @@
 local term = require("term")
 local event = require("event")
 local computer = require("computer")
+local io = require("io")
+local gpu = require("component").gpu
 math.randomseed(os.time())
 
 local function clear()
   term.clear()
+end
+
+local function pause()
+  print("Нажмите любую клавишу...")
+  event.pull("key_down")
 end
 
 local function showMenu()
@@ -13,24 +20,24 @@ local function showMenu()
   print("1. Слот-машина")
   print("2. Выход")
   io.write("Выберите игру: ")
-  return io.read()
+  local choice = io.read()
+  return choice
 end
 
 local function slotMachine()
   clear()
   print("=== Слот-машина ===")
-  print("Нажмите Enter, чтобы запустить.")
-  io.read()
+  print("Нажмите любую клавишу для запуска")
+  event.pull("key_down")
   local symbols = {"BAR", "7", "Cherry", "Diamond", "Lemon"}
   local res = {symbols[math.random(#symbols)], symbols[math.random(#symbols)], symbols[math.random(#symbols)]}
-  print("Результат: "..res[1].." | "..res[2].." | "..res[3])
+  print("Результат: " .. res[1] .. " | " .. res[2] .. " | " .. res[3])
   if res[1] == res[2] and res[2] == res[3] then
     print("Поздравляем, вы выиграли!")
   else
     print("Не повезло, попробуйте снова.")
   end
-  print("Нажмите Enter для возврата в меню.")
-  io.read()
+  pause()
 end
 
 local function main()
@@ -40,10 +47,14 @@ local function main()
       slotMachine()
     elseif choice == "2" then
       break
+    else
+      print("Неверный выбор.")
+      pause()
     end
   end
   clear()
   print("Спасибо за игру!")
+  pause()
 end
 
 main()
