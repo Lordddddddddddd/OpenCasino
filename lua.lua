e1235bb74b12280c1acf137d88eb9a0cdcbec956
Lordddddddddddd <nikicn701@gmail.com>
2025-04-11 15:21:02 +0300

    Update lua.lua

--- lua.lua
+++ lua.lua
@@ -1,6 +1,6 @@
 local internet = require("internet")
 local file = io.open("/home/image.png", "wb")
-for chunk in internet.request("https://raw.githubusercontent.com/user/repo/branch/image.png") do
+for chunk in internet.request("https://github.com/Lordddddddddddd/OpenCasino/blob/main/image.png") do
     file:write(chunk)
 end
 file:close()
